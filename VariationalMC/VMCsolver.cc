7a8923eb75c7aa8462ca1026c12ef76c41c2d183
Paolo Finelli <paolo.finelli@gmail.com>
2019-04-28 16:50:13 +0200

    Add files via upload

---
+++ VariationalMC/VMCsolver.cc
@@ -0,0 +1,238 @@
+#include <armadillo>
+#include <iostream>
+using namespace arma;
+using namespace std;
+double ran2(long *);
+
+class VMCSolver
+{
+public:
+    VMCSolver();
+    void runMonteCarloIntegration();
+
+private:
+    double waveFunction(const mat &r);
+    double localEnergy(const mat &r);
+    int nDimensions;
+    int charge;
+    double stepLength;
+    int nParticles;
+    double h;
+    double h2;
+    long idum;
+    double alpha;
+    int nCycles;
+    mat rOld;
+    mat rNew;
+};
+
+VMCSolver::VMCSolver() :
+    nDimensions(3),
+    charge(2),
+    stepLength(1.0),
+    nParticles(2),
+    h(0.001),
+    h2(1000000),
+    idum(-1),
+    alpha(0.5*charge),
+    nCycles(1000000)
+{
+}
+
+void VMCSolver::runMonteCarloIntegration()
+{
+    rOld = zeros<mat>(nParticles, nDimensions);
+    rNew = zeros<mat>(nParticles, nDimensions);
+    double waveFunctionOld = 0;
+    double waveFunctionNew = 0;
+    double energySum = 0;
+    double energySquaredSum = 0;
+    double deltaE;
+    // initial trial positions
+    for(int i = 0; i < nParticles; i++) {
+        for(int j = 0; j < nDimensions; j++) {
+            rOld(i,j) = stepLength * (ran2(&idum) - 0.5);
+        }
+    }
+    rNew = rOld;
+    // loop over Monte Carlo cycles
+    for(int cycle = 0; cycle < nCycles; cycle++) {
+        // Store the current value of the wave function
+        waveFunctionOld = waveFunction(rOld);
+        // New position to test
+        for(int i = 0; i < nParticles; i++) {
+            for(int j = 0; j < nDimensions; j++) {
+                rNew(i,j) = rOld(i,j) + stepLength*(ran2(&idum) - 0.5);
+            }
+            // Recalculate the value of the wave function
+            waveFunctionNew = waveFunction(rNew);
+            // Check for step acceptance (if yes, update position, if no, reset position)
+            if(ran2(&idum) <= (waveFunctionNew*waveFunctionNew) / (waveFunctionOld*waveFunctionOld)) {
+                for(int j = 0; j < nDimensions; j++) {
+                    rOld(i,j) = rNew(i,j);
+                    waveFunctionOld = waveFunctionNew;
+                }
+            } else {
+                for(int j = 0; j < nDimensions; j++) {
+                    rNew(i,j) = rOld(i,j);
+                }
+            }
+            // update energies
+            deltaE = localEnergy(rNew);
+            energySum += deltaE;
+            energySquaredSum += deltaE*deltaE;
+        }
+    }
+    double energy = energySum/(nCycles * nParticles);
+    double energySquared = energySquaredSum/(nCycles * nParticles);
+    cout << "Energy: " << energy << " Energy (squared sum): " << energySquared << endl;
+}
+
+double VMCSolver::localEnergy(const mat &r)
+{
+    mat rPlus = zeros<mat>(nParticles, nDimensions);
+    mat rMinus = zeros<mat>(nParticles, nDimensions);
+    rPlus = rMinus = r;
+    double waveFunctionMinus = 0;
+    double waveFunctionPlus = 0;
+    double waveFunctionCurrent = waveFunction(r);
+    // Kinetic energy, brute force derivations
+    double kineticEnergy = 0;
+    for(int i = 0; i < nParticles; i++) {
+        for(int j = 0; j < nDimensions; j++) {
+            rPlus(i,j) += h;
+            rMinus(i,j) -= h;
+            waveFunctionMinus = waveFunction(rMinus);
+            waveFunctionPlus = waveFunction(rPlus);
+            kineticEnergy -= (waveFunctionMinus + waveFunctionPlus - 2 * waveFunctionCurrent);
+            rPlus(i,j) = r(i,j);
+            rMinus(i,j) = r(i,j);
+        }
+    }
+    kineticEnergy = 0.5 * h2 * kineticEnergy / waveFunctionCurrent;
+    // Potential energy
+    double potentialEnergy = 0;
+    double rSingleParticle = 0;
+    for(int i = 0; i < nParticles; i++) {
+        rSingleParticle = 0;
+        for(int j = 0; j < nDimensions; j++) {
+            rSingleParticle += r(i,j)*r(i,j);
+        }
+        potentialEnergy -= charge / sqrt(rSingleParticle);
+    }
+    // Contribution from electron-electron potential
+    double r12 = 0;
+    for(int i = 0; i < nParticles; i++) {
+        for(int j = i + 1; j < nParticles; j++) {
+            r12 = 0;
+            for(int k = 0; k < nDimensions; k++) {
+                r12 += (r(i,k) - r(j,k)) * (r(i,k) - r(j,k));
+            }
+            potentialEnergy += 1 / sqrt(r12);
+        }
+    }
+    return kineticEnergy + potentialEnergy;
+}
+
+double VMCSolver::waveFunction(const mat &r)
+{
+    double argument = 0;
+    for(int i = 0; i < nParticles; i++) {
+        double rSingleParticle = 0;
+        for(int j = 0; j < nDimensions; j++) {
+            rSingleParticle += r(i,j) * r(i,j);
+        }
+        argument += sqrt(rSingleParticle);
+    }
+    return exp(-argument * alpha);
+}
+
+/*
+** The function
+**         ran2()
+** is a long periode (> 2 x 10^18) random number generator of
+** L'Ecuyer and Bays-Durham shuffle and added safeguards.
+** Call with idum a negative integer to initialize; thereafter,
+** do not alter idum between sucessive deviates in a
+** sequence. RNMX should approximate the largest floating point value
+** that is less than 1.
+** The function returns a uniform deviate between 0.0 and 1.0
+** (exclusive of end-point values).
+*/
+
+#define IM1 2147483563
+#define IM2 2147483399
+#define AM (1.0/IM1)
+#define IMM1 (IM1-1)
+#define IA1 40014
+#define IA2 40692
+#define IQ1 53668
+#define IQ2 52774
+#define IR1 12211
+#define IR2 3791
+#define NTAB 32
+#define NDIV (1+IMM1/NTAB)
+#define EPS 1.2e-7
+#define RNMX (1.0-EPS)
+
+double ran2(long *idum)
+{
+  int            j;
+  long           k;
+  static long    idum2 = 123456789;
+  static long    iy=0;
+  static long    iv[NTAB];
+  double         temp;
+
+  if(*idum <= 0) {
+    if(-(*idum) < 1) *idum = 1;
+    else             *idum = -(*idum);
+    idum2 = (*idum);
+    for(j = NTAB + 7; j >= 0; j--) {
+      k     = (*idum)/IQ1;
+      *idum = IA1*(*idum - k*IQ1) - k*IR1;
+      if(*idum < 0) *idum +=  IM1;
+      if(j < NTAB)  iv[j]  = *idum;
+    }
+    iy=iv[0];
+  }
+  k     = (*idum)/IQ1;
+  *idum = IA1*(*idum - k*IQ1) - k*IR1;
+  if(*idum < 0) *idum += IM1;
+  k     = idum2/IQ2;
+  idum2 = IA2*(idum2 - k*IQ2) - k*IR2;
+  if(idum2 < 0) idum2 += IM2;
+  j     = iy/NDIV;
+  iy    = iv[j] - idum2;
+  iv[j] = *idum;
+  if(iy < 1) iy += IMM1;
+  if((temp = AM*iy) > RNMX) return RNMX;
+  else return temp;
+}
+#undef IM1
+#undef IM2
+#undef AM
+#undef IMM1
+#undef IA1
+#undef IA2
+#undef IQ1
+#undef IQ2
+#undef IR1
+#undef IR2
+#undef NTAB
+#undef NDIV
+#undef EPS
+#undef RNMX
+
+// End: function ran2()
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    VMCSolver *solver = new VMCSolver();
+    solver->runMonteCarloIntegration();
+    return 0;
+}
